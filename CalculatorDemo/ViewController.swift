75225ba915cb1901f68695a0ad93986e74a2bd2f
markopedu <69790644+markopedu@users.noreply.github.com>
2021-01-07 15:54:56 +0100

    Initial Commit

---
+++ CalculatorDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorDemo
+//
+//  Created by Marko Poikkimäki on 2021-01-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
